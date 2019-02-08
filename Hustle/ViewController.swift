293db2a0aad4ab3afc0aadfb3468125419502e7c
Jay Raval <jay@jays-macbook-pro.local>
2019-02-08 18:07:10 -0500

    Initial Commit

---
+++ Hustle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hustle
+//
+//  Created by Jay Raval on 2/8/19.
+//  Copyright © 2019 Jay Raval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
